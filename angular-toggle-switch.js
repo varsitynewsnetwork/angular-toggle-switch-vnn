f59c5942964304b629b7c7f6ab2f5c45df0f62b0
Erik Alburg <erik@varsitynewsnetwork.com>
2014-07-09 10:20:32 -0400

    modified to allow custom callbacks and switching attributes

--- angular-toggle-switch.js
+++ angular-toggle-switch.js
@@ -4,16 +4,19 @@ angular.module('toggle-switch', ['ng']).directive('toggleSwitch', function () {
     replace: true,
     scope: {
       model: '=',
+      attr_switch: '@',
+      "event": '@',
       disabled: '@',
       onLabel: '@',
       offLabel: '@',
       knobLabel: '@'
     },
-    template: '<div class="switch" ng-click="toggle()" ng-class="{ \'disabled\': disabled }"><div class="switch-animate" ng-class="{\'switch-off\': !model, \'switch-on\': model}"><span class="switch-left" ng-bind="onLabel"></span><span class="knob" ng-bind="knobLabel"></span><span class="switch-right" ng-bind="offLabel"></span></div></div>',
+    template: '<div class="switch" ng-click="toggle()" ng-class="{ \'disabled\': disabled }"><div class="switch-animate" ng-class="{\'switch-off\': !model[attr_switch], \'switch-on\': model[attr_switch]}"><span class="switch-left" ng-bind="onLabel"></span><span class="knob" ng-bind="knobLabel"></span><span class="switch-right" ng-bind="offLabel"></span></div></div>',
     controller: function($scope) {
       $scope.toggle = function toggle() {
         if(!$scope.disabled) {
-          $scope.model = !$scope.model;
+          $scope.model[$scope.attr_switch] = !$scope.model[$scope.attr_switch];
+          $scope.$emit($scope.event, $scope.model);
         }
       };
     },
@@ -22,6 +25,8 @@ angular.module('toggle-switch', ['ng']).directive('toggleSwitch', function () {
       if (!attrs.offLabel) { attrs.offLabel = 'Off'; }
       if (!attrs.knobLabel) { attrs.knobLabel = '\u00a0'; }
       if (!attrs.disabled) { attrs.disabled = false; }
+      if (!attrs.event) { attrs.event = "toggle-switch-change"; }
+      if (!attrs.attr_switch) { attrs.attr_switch = 'enabled'; }
     },
   };
 });
